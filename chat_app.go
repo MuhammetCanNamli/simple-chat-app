4a7382d5869b5143b3243f57c4100600b1f2e581
Can NAMLI <namlimuhammetcan55@gmail.com>
2024-04-01 16:39:23 +0300

    The handleConnections function is complete.

--- chat_app.go
+++ chat_app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -27,6 +28,31 @@ func main() {
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make sure we colse the connection when the function returns
+	defer ws.Close()
+
+	// Register our new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
 }
 
 func handleMessage() {
