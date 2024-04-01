09d8a2f3d4a3d9c54f91d213b4c4c50a4efff3ae
Can NAMLI <namlimuhammetcan55@gmail.com>
2024-04-01 16:40:57 +0300

    Necessary code lines were added according to the handleConnections function in the main scope.

--- chat_app.go
+++ chat_app.go
@@ -24,6 +24,9 @@ func main() {
 	// Create a simple file server
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
+
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
